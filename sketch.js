2f613b14314e652d1cdd8ae226749477e17c1c09
Lucascontaprabyjus <93881090+Lucascontaprabyjus@users.noreply.github.com>
2022-12-15 11:29:11 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,7 +11,8 @@ var fireGroup; //grupo
 var robot;
 var robotimg;
 var robotGroup; //grupo
-var robotHp = 4;
+var robotM = []; //matriz
+var robotHp1 = 4, robotHp2 = 4, robotHp3 = 4, robotHp4 = 4;
 var defesaimg;
 var potencializadorimg;
 var velocidadeimg;
@@ -20,7 +21,12 @@ var defesa;
 var invulnerabilidade;
 var velocidade;
 var potencializador;
-var backgroundimg;
+var fogos = [];
+
+var eliminou1 = false, eliminou2 = false, eliminou3 = false, eliminou4 = false;
+
+var robot1, robot2, robot3, robot4;
+var permissao = true;
 
 function preload()
 {
@@ -34,15 +40,13 @@ velocidadeimg = loadImage ("Sprites/velocidade.png");
 invulnerabilidadeimg = loadImage ("Sprites/invulnerabilidade.png");
 potencializadorimg = loadImage ("Sprites/potencializador.png");
 defesaimg = loadImage ("Sprites/defesa.png");
-backgroundimg = loadImage("Sprites/background.png");
+backgroundimg = loadImage ("Sprites/background.png");
 }
 
 function setup() {
 	//criando tela
 	createCanvas(1200, 700);
 	//criando sprites
-  
-
 	mago = createSprite(40, 550);
 	mago.addImage("mago", magoimg);
   mago.addImage("pulando", pulando);
@@ -58,7 +62,8 @@ function setup() {
 
   //criando grupo
   fireGroup = new Group();
-  robotGroup = new Group();
+
+  //sprites da onda
   wave();
 }
 
@@ -71,122 +76,155 @@ function draw() {
 
   //movimentação do mago
   if (keyDown("w") && mago.y == 483.6){
-
-	mago.velocityY = -10;
-	mago.changeImage("pulando");
-
-  cards();
-  
-
+    mago.velocityY = -10;
+    mago.changeImage("pulando");
   }
   else{
-	setTimeout(()=> {
-    mago.changeImage("mago");
-  }, 200);
-  
-    
+    setTimeout(()=> {
+      mago.changeImage("mago");
+    }, 200);
   }
 
   if (keyDown("d")){
-
-	mago.x += 15;
-
+    mago.x += 15;
   }
 
   if (keyDown("a")){
-
-	mago.x -= 15;
-
+	  mago.x -= 15;
   }
+
   //console.log(mago.y);
   mago.velocityY += 2;
 
+//ataque do mago
+  if (keyWentDown("SPACE")){
+    setTimeout(() => {
+      shooting();
+    }, 900) 
+  }
 
   //movimentação da varinha
-wand1.x = mago.x-20;
-wand1.y = mago.y-30;
+  wand1.x = mago.x-20;
+  wand1.y = mago.y-30;
 
-//if(wand1.rotation < 90 && wand1.rotation > -90) {
-//wand1.pointTo(mouseX, mouseY);
-wand1.rotation = mouseX/6-90;
-//console.log("mouse", mouseX);
-//console.log("mouse wand", wand1.rotation);
+  wand1.rotation = mouseX/6-90;
+  //console.log("mouse", mouseX);
+  //console.log("mouse wand", wand1.rotation);
 
-console.log(robotHp);
-//}
-//wave();
-robotGroup.setSpeedAndDirectionEach(4,mago.x);
-for(i = 0; i<4; i++){
-  if(fireGroup.collide(robotGroup.get(0))){
-robotHp -=1;
-fogo.destroy();
-}
+  console.log("HP" + robotHp1);
 
-}
+  //movimentação dos robos
+  for(i=0; i<4; i++){
+    robotM[i].x = mago.x - 100*i;
+    robotM[i].velocityY = 0.5;
+  }
 
-if(robotHp == 0){
-robot1.destroy();
-robotHp = 4;
+  //colisão dos robos
+  if(fireGroup.collide(robotM[0])){
+    robotHp1 -= 1;
+    fireGroup.destroyEach();
+  }
 
-}
+  if(fireGroup.collide(robotM[1])){
+    robotHp2 -= 1;
+    fireGroup.destroyEach();
+  }
 
+  if(fireGroup.collide(robotM[2])){
+    robotHp3 -= 1;
+    fireGroup.destroyEach();
+  }
+
+  if(fireGroup.collide(robotM[3])){
+    robotHp4 -= 1;
+    fireGroup.destroyEach();
+  }
+
+  //destruição dos robos
+  if(robotHp1 == 0){
+  robotM[0].destroy();
+  eliminou1 = true;
+  console.log("e1: "+eliminou1);
+  robotHp1 = 4;
+  }
+
+  if(robotHp2 == 0){
+    robotM[1].destroy();
+    eliminou2 = true;
+    console.log("e2: "+eliminou2);
+    robotHp2 = 4;
+  }
+
+  if(robotHp3 == 0){
+    robotM[2].destroy();
+    eliminou3 = true;
+    console.log("e3: "+eliminou3);
+    robotHp3 = 4;
+  }
+
+  if(robotHp4 == 0){
+    robotM[3].destroy();
+    eliminou4 = true;
+    console.log("e4: "+eliminou4);
+    robotHp4 = 4;
+  }
+
+  //chamada do cart
+  if(eliminou1 && eliminou2 && eliminou3 && eliminou4){
+     cards();
+  }
 
   drawSprites();
  
 }
 
-function shooting(){
+function shooting(){ 
 fogo = createSprite(wand1.x, wand1.y-55);
 fogo.addImage("fire", fireimg);
+fogo.scale = 0.4;
+fogo.setSpeedAndDirection(10, mouseX/6+180);
 console.log("fogoX", fogo.x);
 console.log("fogoY", fogo.y);
-fogo.lifeTime = 120;
+fogo.lifeTime = 50;
 fireGroup.add(fogo);
-
 }
 function wave(){
- // if(frameCount%120==0){
+ 
+  for(var i = 0; i<4; i++){
+      robot = createSprite (250*i, 100);
+      robot.addAnimation ("robô", robotimg);
+      robot.scale = 0.05;
+      robotM.push(robot);
+    }
+  }
 
-for(var i = 0; i<4; i++){
-//colocar life time
-robot = createSprite (250*i, 100);
-robot.addAnimation ("robô", robotimg);
-robot.scale = 0.07;
-robotGroup.add(robot);
-
-/*robot2 = createSprite (500, 100);
-robot2.addAnimation ("robô", robotimg);
-robot2.scale = 0.07;
-robotGroup.add(robot2);*/
-}
-//robotGroup.setSpeedAndDirectionEach(4,mago.x);
-
-//}
-//}
+  function wave2(){
+ 
+  for(var i = 0; i<10; i++){
+      robot = createSprite (250*i-2, 100);
+      robot.addAnimation ("robô", robotimg);
+      robot.scale = 0.05;
+      robotM.push(robot);
+    }
+  }
 
 function cards(){
-defesa = createImg("Sprites/defesa.png");
-defesa.position(600, 350);
-defesa.size(80, 100);
-defesa.mouseClicked(fcDefesa);
+  console.log("chamou cards");
+  defesa = createImg("Sprites/defesa.png");
+  defesa.position(350, 50);
+  defesa.size(500, 500);
+  defesa.mouseClicked(fcDefesa);
 
+  eliminou1 = false;
+  eliminou2 = false;
+  eliminou3 = false;
+  eliminou4 = false;
 
 }
+
 function fcDefesa(){
-background ("gray");
-console.log("clicou")
+  console.log("clicou");
+  defesa.hide();
+  wave2();
+}
 
-}
-function keyPressed(){
-  if(keyCode == 32){
-    shooting();
-  }
-}
-function keyReleased(){
-  if(keyCode == 32){
-  setTimeout(()=> {
-    fogo.setSpeedAndDirection(10, mouseX/6+180);
-  }, 200);
-  
-}
-}
